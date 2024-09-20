746f3f8d69f50edd198ec70847759f1d7a9012d2
Jacob Hunsaker <jacob.hunsaker96@gmail.com>
2024-09-20 10:21:03 -0500

    change file name to YouTube - Google Chrome

--- Source/FishingFunUI/Properties/AssemblyInfo.cs
+++ Source/FishingFunUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Google Chrome")]
+[assembly: AssemblyTitle("YouTube - Google Chrome")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Google Chrome")]
+[assembly: AssemblyProduct("YouTube - Google Chrome")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
